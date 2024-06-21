fadfd33316a03d683d797e83d693d2a0a8e894df
Mohamed <mohamedjoumoi@gmail.com>
2024-06-21 11:03:33 +0200

    fix homepage route

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import './navbar.scss'
-import {NavLink,  Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export default function Navbar() {
 
@@ -7,21 +7,17 @@ export default function Navbar() {
 
     return (
         <nav className='nav'>
-            
             <ul className='nav_list'>
-                
-                {/* <li className={currentRoute === '/Kasa/' ? 'nav_list_item_active' : 'nav_list_item'}> */}
                 <li className='nav_list_item'>
-                    {/* <Link  to='/'>
-                        Accueil
-                    </Link> */}
-                    <NavLink  to='/kasa'
-            className={({isActive}) => isActive ? 'nav_list_item_active' : 'nav_list_item'}>
+                    <NavLink 
+                        to='/kasa' 
+                        className={({ isActive }) => isActive ? 'nav_list_item_active' : 'nav_list_item'}
+                    >
                         Accueil
                     </NavLink>
                 </li>
                 <li className={currentRoute === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
-                    <Link  to='/about'>
+                    <Link to='/about'>
                         A propos
                     </Link>
                 </li>
